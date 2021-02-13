d793363bb99064c8785433a4d9e699cd384e0044
Fanilo Andrianasolo <andfanilo@users.noreply.github.com>
2021-02-13 15:45:36 +0100

    Write file before rereading?

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import pretty_midi
+import soundfile as sf
 import streamlit as st
 
 st.title(":tada: Hello world !")
 
 midi_data = pretty_midi.PrettyMIDI('toto-africa.mid')
 audio_data = midi_data.fluidsynth()
-st.write(type(audio_data))
-st.audio(audio_data)
+sf.write('stereo_file.wav', naudio_data, 44100, 'PCM_24')
+st.audio('stereo_file.wav')
 
 st.info("End of script, no errors :wink:")
