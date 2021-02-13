c0470962c4e7ba773c8ddf20f5edda58bf593d9f
Fanilo Andrianasolo <andfanilo@users.noreply.github.com>
2021-02-13 15:47:46 +0100

    Don't do typos
    And stop opening the calculator inadvertaneyr...how do you write this

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title(":tada: Hello world !")
 
 midi_data = pretty_midi.PrettyMIDI('toto-africa.mid')
 audio_data = midi_data.fluidsynth()
-sf.write('stereo_file.wav', naudio_data, 44100, 'PCM_24')
+sf.write('stereo_file.wav', audio_data, 44100, 'PCM_24')
 st.audio('stereo_file.wav')
 
 st.info("End of script, no errors :wink:")
