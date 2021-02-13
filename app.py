6f9e71064ac1bec24b75eba07fed2387602d4ad7
Fanilo Andrianasolo <andfanilo@users.noreply.github.com>
2021-02-13 15:30:17 +0100

    Try out pretty-midi audio gen

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import pretty_midi
 import streamlit as st
 
-st.title("hello world")
+st.title(":tada: Hello world !")
+
+midi_data = pretty_midi.PrettyMIDI('toto-africa.mid')
+audio_data = midi_data.synthesize()
+st.audio(audio_data)
+
+st.info("End of script, no errors :wink:")
