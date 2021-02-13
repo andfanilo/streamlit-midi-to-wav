e55217551013bdf389f6088277c92567f0d0a10d
Fanilo Andrianasolo <andfanilo@users.noreply.github.com>
2021-02-13 15:42:11 +0100

    What are you, audio_data?

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ st.title(":tada: Hello world !")
 
 midi_data = pretty_midi.PrettyMIDI('toto-africa.mid')
 audio_data = midi_data.fluidsynth()
+st.write(type(audio_data))
 st.audio(audio_data)
 
 st.info("End of script, no errors :wink:")
