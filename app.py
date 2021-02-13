851e5099ba6ae9a9ef8096284281dc4647c25313
Fanilo Andrianasolo <andfanilo@users.noreply.github.com>
2021-02-13 18:26:12 +0100

    44100 for fs

--- app.py
+++ app.py
@@ -17,6 +17,6 @@ audio_data = midi_data.fluidsynth()
 audio_data = np.int16(audio_data / np.max(np.abs(audio_data)) * 32767 * 0.9) # -- Normalize for 16 bit audio https://github.com/jkanner/streamlit-audio/blob/main/helper.py
 
 virtualfile = io.BytesIO()    
-wavfile.write(virtualfile, int(fs), audio_data)
+wavfile.write(virtualfile, 44100, audio_data)
     
 st.audio(virtualfile)
